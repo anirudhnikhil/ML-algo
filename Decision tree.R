c30cd86858c2e98a8873d7db2c9ac13d8ca6b7ed
anirudhnikhil <anirudh.nikhil@gmail.com>
2018-12-19 16:07:19 -0500

    Update Decision tree.R

--- Decision tree.R
+++ Decision tree.R
@@ -1,27 +1,6 @@
-mower.df <- read.csv("RidingMowers.csv")
-library(rpart)
-library(rpart.plot)
+##This code creates a Decision Tree Clasifier for Loan Acceptance based on several customer characteristics  
 
-# use rpart() to run a classification tree.
-# define rpart.control() in rpart() to determine the depth of the tree.
-class.tree <- rpart(Ownership ~., data = mower.df,
-                    control = rpart.control(maxdepth = 2), method = "class")
-
-# plot tree
-# use prp() to plot the tree. You can control plotting parameters such as color, shape, 
-# and information displayed (which and where).
-prp(class.tree, type = 1, extra=1, split.font = 1, varlen = -10)
-
-
-# Create a full grown tree.
-# define rpart.control() in rpart() to determine the depth of the tree.
-class.tree <- rpart(Ownership ~., data = mower.df, control = rpart.control(minsplit = 2), method = "class")
-
-# plot tree
-# use prp() to plot the tree. You can control plotting parameters such as color, shape, 
-# and information displayed (which and where).
-prp(class.tree, type = 1, extra=1, split.font = 1, varlen = -10, digits = 5)
-
+rm(list=ls())
 
 
 
